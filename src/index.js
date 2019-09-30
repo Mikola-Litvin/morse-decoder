f69f711e669c3afd6c2db0bc34d73ac7553facde
Nikolai Shkrabkov <grr_by@mail.ru>
2019-09-30 23:35:29 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -38,7 +38,33 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = [];
+  let letter = [];
+  let code = "";
+  let pattern = /^\*/;
+  for (let i = 0; i < expr.length; i += 10) {
+    arr.push(expr.substr(i, 10));
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (pattern.test(arr[i])) {
+      arr[i] = " ";
+    }
+    else {
+      for (let j = 0; j < arr[i].length; j += 2) {
+        code += arr[i].substr(j, 2);
+        if (code === "10") {
+          letter.push(".");
+        }
+        if (code === "11") {
+          letter.push("-");
+        }
+        code = "";
+      }
+      arr[i] = MORSE_TABLE[letter.join('')];
+      letter.length = 0;
+    }
+  }
+  return arr.join('');
 }
 
 module.exports = {
